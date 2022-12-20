cac8d2b16242e15154bf0dded762acb26c88620c
Jacques Roux <jacquesf.roux@gmail.com>
2022-12-20 16:13:58 +0200

    Adding sequelize package and defining Product Model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'express-shop',
-  password: ''
+const sequelize = new Sequelize('express-shop', 'root', 'karools', {
+  dialect: 'mysql',
+  host: 'localhost'
 })
 
-module.exports = pool.promise()
+module.exports = sequelize

8088f2249ef7faf74c34cd241bbe68e3dcbbd330
Jacques Roux <jacquesf.roux@gmail.com>
2022-12-28 09:06:11 +0200

    Connecting MongoDB to project

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const { Sequelize } = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('express-shop', 'root', 'karools', {
-  dialect: 'mysql',
-  host: 'localhost'
-})
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://jacquesroux:xWt394nEGH32bK6O@cluster0.khugxo3.mongodb.net/?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected!')
+    callback(client)
+  })
+  .catch(err => console.log(err))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect

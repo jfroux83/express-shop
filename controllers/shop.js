b46822e796fbbbaf7f1903cfc7e951976bd3aab1
Jacques Roux <jacquesf.roux@gmail.com>
2023-01-03 12:50:13 +0200

    Finalize changing code to using mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product')
+const Order = require('../models/order')
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -80,16 +81,35 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
-  let fecthedCart
-  req.user.addOrder()
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return {
+          quantity: i.quantity,
+          product: { ...i.productId._doc }
+        }
+      })
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      })
+      return order.save()
+    })
     .then(result => {
+      return req.user.clearCart()
+    })
+    .then(() => {
       res.redirect('/orders')
     })
     .catch(err => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders()
+  Order.find({  'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         pageTitle: 'Your Orders',
--- routes/shop.js
+++ routes/shop.js
@@ -17,9 +17,9 @@ router.post('/cart', shopController.postCart)
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct)
 
-// router.post('/create-order', shopController.postOrder)
+router.post('/create-order', shopController.postOrder)
 
-// router.get('/orders', shopController.getOrders)
+router.get('/orders', shopController.getOrders)
 
 // router.get('/checkout', shopController.getCheckout)
 

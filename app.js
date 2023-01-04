698b75ca4feec2940366e399ac1bdf17d363549f
Jacques Roux <jacquesf.roux@gmail.com>
2023-01-04 17:29:10 +0200

    Adding authentication to project

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Jacques',
-          email: 'jacques@phoenexis.co.za',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,19 @@
+const bcrypt = require('bcryptjs')
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
     isAuthenticated: false
   });
 };
@@ -19,6 +29,31 @@ exports.postLogin = (req, res, next) => {
       });
     })
     .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+  const confirmPassword = req.body.confirmPassword
+  User.findOne({email: email})
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup')
+      }
+      return bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          })
+          return user.save()
+        })
+        .then(result => {
+          res.redirect('/login')
+        })
+    })
+    .catch(err => console.log(err))
 };
 
 exports.postLogout = (req, res, next) => {

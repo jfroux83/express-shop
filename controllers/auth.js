64eb5bbfc47d7889809b60b4f49497b8cdca2838
Jacques Roux <jacquesf.roux@gmail.com>
2023-01-05 11:04:02 +0200

    Finalize authentication with flash messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,30 +3,59 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    errorMessage: message
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('63b523eee7cc426260bb8ad3')
+  const email = req.body.email
+  const password = req.body.password
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        req.flash('error', 'Invalid email or password.')
+        return res.redirect('/login')
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/')
+            });
+          }
+          req.flash('error', 'Invalid email or password.')
+          res.redirect('/login')
+        })
+        .catch(err => {
+          console.log(err)
+          res.redirect('/login')
+        })
     })
     .catch(err => console.log(err));
 };
@@ -38,6 +67,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({email: email})
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'E-Mail exists already, please pick a different one.')
         return res.redirect('/signup')
       }
       return bcrypt.hash(password, 12)
--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,6 @@
 exports.get404 = (req, res, next) => {
   res.status(404).render('404', {
     pageTitle: 'Page Not Found',
-    path: '/404',
-    isAuthenticated: req.session.isLoggedIn
+    path: '/404'
   });
 };

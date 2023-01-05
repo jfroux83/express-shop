e9b47aadca73e815684fd24e844fefcadbdc4df1
Jacques Roux <jacquesf.roux@gmail.com>
2023-01-05 17:22:56 +0200

    Adding nodemailer & reset password process

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
+const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
+const sendgridTransport = require('nodemailer-sendgrid-transport')
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: ''
+  }
+}))
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error')
@@ -81,7 +90,14 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login')
+          return transporter.sendMail({
+            to: email,
+            from: 'info@phoenexis.co.za',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          })
         })
+        .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 };
@@ -92,3 +108,51 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.')
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save()
+      })
+      .then(result => {
+        res.redirect('/')
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'info@phoenexis.co.za',
+          subject: 'Password reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `
+        })
+      })
+      .catch(err => console.log(err))
+  })
+}

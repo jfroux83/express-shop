70b680d7178a032430e0fc4816f3469babed52dc
Jacques Roux <jacquesf.roux@gmail.com>
2023-01-09 16:15:22 +0200

    finalize adding validation to post requests

--- routes/auth.js
+++ routes/auth.js
@@ -15,10 +15,12 @@ router.post(
   [
     body('email')
       .isEmail()
-      .withMessage('Please enter a valid email address.'),
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
     body('password', 'Password has to be valid.')
       .isLength({ min: 5 })
       .isAlphanumeric()
+      .trim()
   ],
   authController.postLogin
 );
@@ -41,14 +43,18 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       'password',
       'Please enter a password with only numbers and text and at least 5 characters.'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body('confirmPassword').custom((value, { req }) => {
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+    .trim()
+    .custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error('Passwords have to match!');
       }

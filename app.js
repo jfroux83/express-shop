65062341c61641d1301922ff1a4432f6e2af88dd
Jacques Roux <jacquesf.roux@gmail.com>
2023-01-10 09:11:58 +0200

    finalize error handling

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.use(csrfProtection)
 app.use(flash())
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
+
+app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
@@ -54,15 +60,9 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err)
+      next(new Error(err))
     });
 });
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn
-  res.locals.csrfToken = req.csrfToken()
-  next()
-})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -73,7 +73,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...)
-  res.redirect('/500')
+  // res.redirect('/500')
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 })
 
 mongoose
